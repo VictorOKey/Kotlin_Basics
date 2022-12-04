c71d2b4ab3967898e0907be31b8349047f097701
VictorOKey <82977567+VictorOKey@users.noreply.github.com>
2022-12-05 02:08:07 +0300

    Update Main.kt

--- Kotlin_Basics/src/main/kotlin/Main.kt
+++ Kotlin_Basics/src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
 import java.time.Year
 import java.util.StringJoiner
 
-/*fun main()
+/*fun main() //Вывод информации
 {
     println("Ivan")
     println(35)
     println(true)
 }*/
-/*fun main()
+/*fun main() // Работа с переменными
 {
     var number: Int = 5;
     var char: Char = 'F';
@@ -16,13 +16,13 @@ import java.util.StringJoiner
     val read: Int = 67;
     print(str)
 }*/
-/*fun main()
+/*fun main() // Разделение числа на символы
 {
     val number: String = readLine().toString()
     val result = number.toCharArray().joinToString (",")
     print(result)
 }*/
-/*fun main()
+/*fun main() // Работа с числами
 {
     val number = 2642
     var year: Int = number/365
@@ -31,12 +31,12 @@ import java.util.StringJoiner
 
     println("Лет: $year Недель: $weak Дней: $day")
 }*/
-/*fun main()
+/*fun main() // Нахождение числа
 {
     var num: Int = readLine()!!.toInt()
     print("$num${num*2}")
 }*/
-fun main()
+fun main() // Работа с переменными
 {
     val name: String = readLine().toString();
     val age: Int = readLine()!!.toInt();
